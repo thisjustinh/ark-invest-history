10a9b53db689d8b7f95fe69698d9b2ee09c18f7b
Justin Huang <xinglongjh@gmail.com>
2021-01-01 13:35:42 -0500

    Tweaked CSV processing for transactions & combining

---
+++ src/combine_transactions.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+arkf = pd.read_csv('../transactions/ARKF.csv')
+arkk = pd.read_csv('../transactions/ARKK.csv')
+arkg = pd.read_csv('../transactions/ARKG.csv')
+arkq = pd.read_csv('../transactions/ARKQ.csv')
+arkw = pd.read_csv('../transactions/ARKW.csv')
+
+latest = pd.concat([arkf, arkk, arkg, arkq, arkw])
+latest.to_csv('../transactions/latest.csv', index=False)
+
+# print(latest)
+# print(arkf.shape, arkk.shape, arkg.shape, arkq.shape, arkw.shape)
+
+master = pd.read_csv('../transactions/master.csv')
+
+if master['date'][-1] != latest['date'][0]:
+    master = pd.concat([master, latest])
+    master.to_csv('../transactions/master.csv', index=False)
+    # print(master)
+
--- src/process.py
+++ src/process.py
@@ -7,20 +7,17 @@ holdings = StringIO(sys.stdin.read())
 new = pd.read_csv(holdings, error_bad_lines=False)
 new = new.iloc[:-3]
 
-print(new)
-
 fund = new['fund'][0]
 date = new['date'][0]
 old = pd.read_csv(f'../fund-holdings/{fund}.csv')
 
 if old['date'][0] != new['date'][0]:
-    new.to_csv(f'../fund-holdings/{fund}.csv')
+    # new.to_csv(f'../fund-holdings/{fund}.csv', index=False)
 
     transactions = []
     for index, row in new.iterrows():
         try:
             old_row = old.loc[old['company'] == row['company']]
-            print(old_row)
         except KeyError:
             # completely new holding
             transactions.append([
@@ -34,14 +31,13 @@ if old['date'][0] != new['date'][0]:
                 round(float(row['weight(%)']), 2)
             ])
             continue
-        print('nani')
         if float(old_row['shares'].iloc[0]) < float(row['shares']):
             direction = 'Buy'
         elif float(old_row['shares'].iloc[0]) > float(row['shares']):
             direction = 'Sell'
         else:
             continue
-        print('what?')            
+   
         transactions.append([
             date, 
             fund, 
@@ -52,14 +48,11 @@ if old['date'][0] != new['date'][0]:
             round(abs(float(row['shares']) - float(old_row['shares'].iloc[0])), 2),
             round(abs(float(row['weight(%)']) - float(old_row['weight(%)'].iloc[0])), 2)
         ])
-        print(transactions[-1])
     
     # Second pass for checking if anything has been completely sold
-    print('SPOTIFY TECHNOLOGY SA' in new['company'])
     for index, row in old.iterrows():
         if not new['company'].str.contains(row['company']).any():
             # completely sold holding
-            print(index)
             transactions.append([
                 date, 
                 fund, 
@@ -71,7 +64,8 @@ if old['date'][0] != new['date'][0]:
                 round(float(row['weight(%)']), 2)
             ])
 
+    # Check nonempty
     if len(transactions):
         transactions_csv = pd.DataFrame(transactions, columns=['date', 'fund', 'direction', 'company', 'ticker', 'cusip', 'shares', 'weight(%)'])
     
-        transactions_csv.to_csv(f'../transactions/{fund}.csv')
+        transactions_csv.to_csv(f'../transactions/{fund}.csv', index=False)
