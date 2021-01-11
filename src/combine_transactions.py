9e6738fbedd0c059a488a04146af826116e7572c
Justin Huang <xinglongjh@gmail.com>
2021-01-11 01:36:36 -0500

    More folder errors :/

--- src/combine_transactions.py
+++ src/combine_transactions.py
@@ -7,12 +7,12 @@ arkq = pd.read_csv('../fund-holdings/deltaARKQ.csv')
 arkw = pd.read_csv('../fund-holdings/deltaARKW.csv')
 
 latest = pd.concat([arkf, arkk, arkg, arkq, arkw])
-latest.to_csv('../transactions/latest.csv', index=False)
+latest.to_csv('../fund-holdings/latest.csv', index=False)
 
 # print(latest)
 # print(arkf.shape, arkk.shape, arkg.shape, arkq.shape, arkw.shape)
 
-master = pd.read_csv('../transactions/master.csv')
+master = pd.read_csv('../fund-holdings/master.csv')
 
 if master['date'].iloc[-1] != latest['date'].iloc[0]:
     master = pd.concat([master, latest])
